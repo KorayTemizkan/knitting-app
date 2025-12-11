d0853ff7f23789e9d78e2a333681d62b4ca2162c
Koray <temizkankoray1923@gmail.com>
2025-12-11 18:07:00 +0300

    ilk taslak

---
+++ knitting_app/lib/controllers/app_view.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class AppView extends StatelessWidget {
+  final StatefulNavigationShell navigationShell;
+  const AppView({super.key, required this.navigationShell});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Knitting App"),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+        elevation: 0,
+      ),
+
+      body: navigationShell,
+      bottomNavigationBar: NavigationBarTheme(
+       
+        data: NavigationBarThemeData(
+          backgroundColor: Colors.white,
+        ),
+
+        child: NavigationBar(
+          
+          selectedIndex: navigationShell.currentIndex,
+          onDestinationSelected: navigationShell.goBranch,
+
+          destinations: [
+            _menuItem(
+              context,
+
+              index: 0,
+              currentIndex: navigationShell.currentIndex,
+              label: 'Feed',
+              icon: Icons.feed,
+            ),
+
+            _menuItem(
+              context,
+
+              index: 1,
+              currentIndex: navigationShell.currentIndex,
+              label: 'Explore',
+              icon: Icons.feed,
+            ),
+
+            _menuItem(
+              context,
+
+              index: 2,
+              currentIndex: navigationShell.currentIndex,
+              label: 'Community',
+              icon: Icons.feed,
+            ),
+
+            _menuItem(
+              context,
+
+              index: 3,
+              currentIndex: navigationShell.currentIndex,
+              label: 'Profile',
+              icon: Icons.feed,
+            ),
+            
+          ]
+        ),
+      ),
+    );
+  }
+
+
+  Widget _menuItem(
+    BuildContext context, {
+    required int index,
+    required int currentIndex,
+    required String label,
+    required IconData icon,
+  }) {
+    return NavigationDestination(
+      icon: Icon(
+        icon,
+        color: currentIndex == index ? Colors.black : Colors.red,
+      ),
+      label: label,
+    );
+  }
+
+
+}
---
+++ knitting_app/lib/controllers/router.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:knitting_app/controllers/app_view.dart';
+import 'package:knitting_app/views/community_view/community_view.dart';
+import 'package:knitting_app/views/feed_view/feed_view.dart';
+import 'package:knitting_app/views/profile_view/profile_view.dart';
+import 'package:knitting_app/views/search_view/search_view.dart';
+
+// Tüm uygulama geçişleri için tek bir navigator anahtarı oluşturuyoruz.
+final _routerKey = GlobalKey<NavigatorState>();
+
+class AppRoutes {
+  AppRoutes._(); // Bu sınıfın örneği oluşturulmasın diye özel yapıcı fonksiyonumuz, oluşturmaya gerek yok çünkü sabitlerimiz zaten var
+
+  static const String feed = '/'; // static sayesinde nesne oluşturmadan doğruca ileride AppRoutes.search diye erişebiliyoruz
+  static const String search = '/search';
+  static const String community = '/community';
+  static const String profile = '/profile';
+
+  static const String settings = "/settings";
+  static const String login = '/login';
+}
+
+final router = GoRouter(
+  navigatorKey: _routerKey, // uygulamanın herhangi bir yerinden navigatorKey.currentState üzerinden navigator'e erişme imkanı verir
+  initialLocation: AppRoutes.feed,
+
+  routes: [_bottomBar()], // bottom bar ile alt barı tek seferde hallediyoruz
+);
+
+StatefulShellRoute _bottomBar() {
+  // her bir sekme için bir statefulshellbranch yaratıyor ve bunları indexlenmiş stack olarak tutuyor, yani sekmeler aynı anda çalışıyor ama arka planda bekliyor.
+  return StatefulShellRoute.indexedStack(
+    
+    builder: (context, state, navigationShell) =>
+        AppView(navigationShell: navigationShell),
+
+    branches: [
+      StatefulShellBranch(
+        routes: [
+          GoRoute(
+            path: AppRoutes.feed,
+            builder: (context, state) => FeedView(),
+          ),
+        ],
+      ),
+
+      StatefulShellBranch(
+        routes: [
+          GoRoute(
+            path: AppRoutes.search,
+            builder: (context, state) => const SearchView(),
+          ),
+        ],
+      ),
+
+      StatefulShellBranch(
+        routes: [
+          GoRoute(
+            path: AppRoutes.community,
+            builder: (context, state) => const CommunityView(),
+          ),
+        ],
+      ),
+
+      StatefulShellBranch(
+        routes: [
+          GoRoute(
+            path: AppRoutes.profile,
+            builder: (context, state) => const ProfileView(),
+          ),
+        ],
+      ),
+    ],
+  );
+}
